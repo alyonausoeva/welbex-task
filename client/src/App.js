5ec8399ecb71a83b71c61781fefc991cb1ace3dd
alyonausoeva <alyonausus@gmail.com>
2021-02-09 17:39:28 +0300

    Improve Filter

--- client/src/App.js
+++ client/src/App.js
@@ -18,26 +18,26 @@ function App() {
   const [columnTitle, setColumnTitle] = useState("");
 
   const getFiltredRows = () => {
-    if (!searchText || !columnTitle || !condition) {
-      return rows;
+    if (searchText && columnTitle && condition) {
+      return rows.filter((item) => {
+        if (condition === "equal") {
+          return item[columnTitle].toString() === searchText;
+        }
+        if (condition === "less") {
+          return item[columnTitle] < searchText;
+        }
+        if (condition === "greater") {
+          return item[columnTitle] > searchText;
+        }
+        if (condition === "contain") {
+          return item[columnTitle]
+            .toLowerCase()
+            .includes(searchText.toLowerCase());
+        }
+        return <h2> Записи отсутсвуют </h2>;
+      });
     }
-
-    return rows.filter((item) => {
-      console.log(typeof item[columnTitle].toString());
-      if (condition === "equal") {
-        return item[columnTitle] == searchText;
-      }
-      if (condition === "less") {
-        return item[columnTitle] < searchText;
-      }
-      if (condition === "greater") {
-        return item[columnTitle] > searchText;
-      }
-      if (condition === "contain") {
-        return item[columnTitle].toString();
-      }
-      return rows;
-    });
+    return rows;
   };
 
   const filtredRows = getFiltredRows();
